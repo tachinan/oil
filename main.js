39082f9729749d0c0f03e925a8d410c576e9f36e
Natnicha433 <113772355+tachinan@users.noreply.github.com>
2022-11-21 11:19:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let mysql = require('mysql');
+
+let PORT = process.env.PORT || 3021;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    return res.send({
+        message: '64050283, 64050433, 64050602'
+    })
+})
+
+
+//connect sql
+let dbCon = mysql.createConnection(
+    {
+        host :'localhost',
+        user: 'root',
+        database:'csv_db 6'
+})
+dbCon.connect();
+
+//retrive all
+app.get('/oil',(req, res) => {
+       dbCon.query('SELECT * FROM f0f5b4f0dd4c3e7b', (error, results, fields) => {
+       if (error) throw error;
+       let message = ""
+       if(results === undefined || results.length == 0){
+        message = "oil table is empty"
+       }else{
+        message = "successfully";
+       }
+       return res.send({/*error: false, */data: results/*, message: message*/});
+    })
+    })
+app.listen(PORT, () => {
+    console.log('node is running App http://localhost:3021');
+})
+
+module.exports = app;
